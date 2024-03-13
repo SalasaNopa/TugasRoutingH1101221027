1c3eec88329f8f448d9fca5a76f6b1d93f6f8930
SalasaNopa <miawboo17@gmail.com>
2024-03-13 15:55:39 +0700

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,11 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Website Pribadi</title>
+    <link rel="stylesheet" href="login.css">
 </head>
+
 <body>
-    <h1>Login</h1>
+    <div class="container">
+        <div class="login">
+            <form action="home.html">
+                <h1>Login</h1>
+                <hr>
+                <label for="">Email</label>
+                <input type="text" placeholder="example@gmail.com" required>
+                <label for="">Password</label>
+                <input type="password" placeholder="Password" required>
+                <button type="Submit">Submit</button>
+                <p>
+                    <a href="#">Forgot Password?</a>
+                </p>
+            </form>
+        </div>
+    </div>
 </body>
+
 </html>
